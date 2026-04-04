1045b27c4676ebd1ecabdbd532ff2c081026cb1c
Kumarshubham21 <7979shubhamkumar@gmail.com>
2026-04-04 16:32:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -318,5 +318,4 @@ def profile():
     return render_template('profile.html', user=user, stats=stats, success=success_msg, error=error_msg)
 
 if __name__=='__main__':
-    init_db()
     app.run(host='0.0.0.0',port=5002,debug=True)
